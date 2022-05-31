ba40499199ce7684f26ad89e9524f6722256f4ae
Dongyeop <kitty-777@naver.com>
2022-06-01 00:15:16 +0900

    20220601
    이유모를 커밋

---
+++ Assets/01.Script/PlayerMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    
+
+    private void Update()
+    {
+        
+    }
+
+    private void PlayerMovment()
+    {
+    }
+}
