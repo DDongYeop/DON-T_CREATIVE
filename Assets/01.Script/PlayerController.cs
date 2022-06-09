0857e8242595a815302f459b03da5c7ab1c63bbf
Dongyeop <kitty-777@naver.com>
2022-06-09 23:41:10 +0900

    20220609
    Plyaer can't get off the screen

--- Assets/01.Script/PlayerController.cs
+++ Assets/01.Script/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private StageData stageData;
+
     Movement movement;
+
 
     private void Awake()
     {
@@ -17,5 +20,12 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
 
         movement.MoveTo(new Vector3(x, y, 0));
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, stageData.LimitMin.x, stageData.LimitMax.x)
+          , Mathf.Clamp(transform.position.y, stageData.LimitMin.y, stageData.LimitMax.y), 0);
     }
 }
