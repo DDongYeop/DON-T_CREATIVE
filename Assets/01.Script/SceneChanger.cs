b8793d5df57045311868643dbcf3c6d37dfa5244
Dongyeop <kitty-777@naver.com>
2022-06-09 22:10:02 +0900

    20220609
    Choose Scene

--- Assets/01.Script/SceneChanger.cs
+++ Assets/01.Script/SceneChanger.cs
@@ -7,6 +7,12 @@ public class SceneChanger : MonoBehaviour
 {
     [SerializeField] private int SceneNumber;
 
+    [SerializeField] private int SceneNumber1;
+    [SerializeField] private int SceneNumber2;
+    [SerializeField] private int SceneNumber3;
+    [SerializeField] private int SceneNumber4;
+    [SerializeField] private int SceneNumber5;
+
     public void Exit()
     {
         Application.Quit();
@@ -16,5 +22,30 @@ public class SceneChanger : MonoBehaviour
     public void PlayerScene()
     {
         SceneManager.LoadScene(SceneNumber);
+    }
+
+    public void PlayerScene1()
+    {
+        SceneManager.LoadScene(SceneNumber1);
+    }
+
+    public void PlayerScene2()
+    {
+        SceneManager.LoadScene(SceneNumber2);
+    }
+
+    public void PlayerScene3()
+    {
+        SceneManager.LoadScene(SceneNumber3);
+    }
+
+    public void PlayerScene4()
+    {
+        SceneManager.LoadScene(SceneNumber4);
+    }
+
+    public void PlayerScene5()
+    {
+        SceneManager.LoadScene(SceneNumber5);
     }
 }
