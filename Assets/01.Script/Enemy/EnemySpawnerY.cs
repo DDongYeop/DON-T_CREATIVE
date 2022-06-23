35ed0c5d8a7c1e5cd8d59424e763616df0f571cd
Dongyeop <kitty-777@naver.com>
2022-06-23 12:40:55 +0900

    20220623
    Add stage2 enemy

---
+++ Assets/01.Script/Enemy/EnemySpawnerY.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerY : MonoBehaviour
+{
+    [SerializeField] private StageData _stageData;
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private float _spawnTime;
+
+    private void Awake()
+    {
+        StartCoroutine("SpawnEnemy");
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            float positionY = Random.Range(_stageData.LimitMin.y, _stageData.LimitMax.y);
+            Instantiate(_enemy, new Vector3(_stageData.LimitMax.y + 5.5f, positionY, 0.0f), Quaternion.identity);
+            yield return new WaitForSeconds(_spawnTime);
+        }
+    }
+}
--- Assets/01.Script/Player/PlayerController.cs
+++ Assets/01.Script/Player/PlayerController.cs
@@ -18,8 +18,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
         _movement.MoveTo(new Vector3(x, y, 0));
 
